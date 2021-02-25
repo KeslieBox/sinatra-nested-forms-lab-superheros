0ea52a169431ad5de542a7cae046476a8336aeef
KeslieBox <kesliebox@gmail.com>
2021-02-24 21:13:13 -0600

    commit tree bdd261101d207fef1cea1d134828e92c74fbe87a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :new
+    end
+
+    get '/teams' do
+        erb :new
+    end
+
+    post '/teams' do
+        # binding.pry
+       @team = params[:team]
+       @members = params[:team][:members]
+    #    binding.pry
+        erb :team
+    end
+
 
 end
